11cbc663d3bd767496094fcf7d2993ab83152861
johnlatona <johnbl24@aol.com>
2018-07-30 16:03:29 -0400

    split and merge

--- mergesort.js
+++ mergesort.js
@@ -4,3 +4,28 @@ function split(wholeArray) {
   let second = wholeArray.slice(midPoint);
   return [first, second];
 }
+
+function merge(array1, array2) {
+  let resultArr = [];
+  let i = 0;
+  let j = 0;
+  while(i < array1.length && j < array2.length) {
+    let iCurrent = array1[i];
+    let jCurrent = array2[j];
+    if(iCurrent < jCurrent) {
+      resultArr.push(iCurrent);
+      i++;
+    }
+    else {
+      resultArr.push(jCurrent);
+      j++;
+    }
+  }
+  if(i < array1.length) {
+    return resultArr.concat(array1.slice(i));
+  }
+  else if(j < array2.length) {
+    return resultArr.concat(array2.slice(j));
+  }
+  return resultArr;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,8 +1,6 @@
+
+
 describe('Split Array function', function() {
-  let wholeArrayEven;
-  let wholeArrayOdd;
-  let resultEven;
-  let resultOdd;
   beforeEach(function() {
     wholeArrayEven = [1,3,5,47,9,32,14,6,8,10];
     wholeArrayOdd = [1,3,5,63,7,34,9];
@@ -23,5 +21,32 @@ describe('Split Array function', function() {
   it('returns the correct two arrays', function() {
     expect(resultEven).toEqual([[1,3,5,47,9],[32,14,6,8,10]]);
     expect(resultOdd).toEqual([[1,3,5],[63,7,34,9]]);
-  })
-})
+  });
+});
+
+describe('Merge function', function() {
+  let sortedEven, sortedOdd;
+  beforeEach(function() {
+    sortedEven = [1,3,5,6,8,9,10,14,32,47];
+    sortedOdd = [1,3,5,7,9,34,63];
+  });
+
+  it('merges two sorted arrays into one array', function() {
+    let array1Even = [1,5,8,10,14];
+    let array2Even = [3,6,9,32,47];
+    let array1Odd = [1,7,34];
+    let array2Odd = [3,5,9,63];
+    expect(merge(array1Even, array2Even)).toEqual(sortedEven);
+    expect(merge(array1Odd, array2Odd)).toEqual(sortedOdd);
+  });
+
+  it('merges two arrays with one or two elements correctly', function() {
+    let singleArrayOne = [2];
+    let singleArrayTwo = [5];
+    let doubleArrayOne = [2,8];
+    let doubleArrayTwo = [6,9];
+    expect(merge(singleArrayOne, singleArrayTwo)).toEqual([2,5]);
+    expect(merge(doubleArrayOne, doubleArrayTwo)).toEqual([2,6,8,9]);
+    expect(merge(singleArrayOne, doubleArrayTwo)).toEqual([2,6,9]);
+  });
+});
